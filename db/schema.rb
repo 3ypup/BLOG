9b1b28dcc1d99e59cf0db6cabd19ecdc7c1b566e
3ypup <posilay.pochashe@gmail.com>
2017-12-29 14:55:09 +0300

    add contacts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171222110014) do
+ActiveRecord::Schema.define(version: 20171229114643) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20171222110014) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
